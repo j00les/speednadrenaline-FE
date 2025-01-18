87dbadbdce0ba0a6f382cd70b3f6f3e2b155ec9f
Nabiel Alif <nabiel.alif01@gmail.com>
2025-01-18 12:37:14 +0700

    feat: update

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -19,35 +19,30 @@ const Row = (props) => {
     return blockColor;
   };
 
-  const sortedDataByLapTime = leaderboardData
-    .slice()
-    .sort((a, b) => a.lapTime - b.lapTime)
-    .slice(0, 21);
-
   return (
     <>
-      {sortedDataByLapTime.map((record, index) => {
+      {leaderboardData.map((record, index) => {
         const { driverName, lapTime, carName, carType, gapToFirst } = record;
+
         const position = index + 1;
         const positionMargin = position < 10 && 'ml-[.8rem]';
         const styling = position % 2 === 0 && 'bg-[#D4D4D4]';
-
         const blockColor = getColorForCarType(carType);
 
         return (
           <tr key={index} className={`font-sugo uppercase font-medium ${styling}`}>
-            <td className="flex gap-1.5 mt-2  w-[11rem]">
-              <span className={`text-2xl font-titillium font-medium ${positionMargin}`}>
+            <td className="flex gap-1.5 mt-2  w-[13rem]">
+              <span className={`text-[1.4rem] font-titillium font-medium ${positionMargin}`}>
                 {position}
               </span>
-              <span className={`w-[.7rem] ${blockColor}`}></span>
-              <span className={`ml-1 text-3xl tracking-tight`}>{driverName}</span>
+              <span className={`w-[.5rem] ${blockColor}`}></span>
+              <span className={`text-[1.7rem]  tracking-tight`}>{driverName}</span>
             </td>
-            <td className="font-titillium font-semibold text-center pr-[2rem]">{lapTime}</td>
-            <td className="text-[1.5rem] text-center pr-[1.5rem] font-titillium font-semibold">
+            <td className="font-titillium text-[1.4rem] font-semibold text-center">{lapTime}</td>
+            <td className="text-[1.4rem] text-center pr-[.5rem] font-titillium font-semibold">
               {gapToFirst}
             </td>
-            <td className="text-[1.5rem] text-left pl-[.8rem] font-titillium font-semibold">
+            <td className="text-[1.4rem] text-left pl-[.9rem] font-titillium font-semibold">
               {carName}
             </td>
           </tr>
--- src/pages/Input.jsx
+++ src/pages/Input.jsx
@@ -1,77 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-import { useWebSocket } from '../context/WebSocketContext';
 import Dropdown from '../components/Dropdown';
 import Table from '../components/Table';
-import { leaderboardData } from '../constants';
+import { useWebSocket } from '../context/WebsocketContext';
 
 const Input = () => {
+  const { data: leaderboardData, sendData } = useWebSocket(); // Get leaderboardData from context
+  const CAR_OPTIONS = ['FWD', 'AWD', 'RWD'];
+
   const [carName, setCarName] = useState('');
   const [driverName, setDriverName] = useState('');
   const [lapTime, setLapTime] = useState('');
   const [carType, setCarType] = useState('');
-  // const [leaderboardData, setLeaderboardData] = useState([]);
 
-  const CAR_OPTIONS = ['FWD', 'AWD', 'RWD'];
-  const { socket } = useWebSocket();
-
-  const handleCarNameChange = (event) => {
-    setCarName(event.target.value);
-  };
-
-  const handleLapTimeChange = (event) => {
-    setLapTime(event.target.value);
-  };
-
-  const handleDriverNameChange = (event) => {
-    setDriverName(event.target.value);
-  };
-
-  const handleCarTypeChange = (event) => {
-    setCarType(event.target.value);
-  };
+  const handleCarNameChange = (event) => setCarName(event.target.value);
+  const handleLapTimeChange = (event) => setLapTime(event.target.value);
+  const handleDriverNameChange = (event) => setDriverName(event.target.value);
+  const handleCarTypeChange = (event) => setCarType(event.target.value);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const newRecord = {
+      driverName,
+      lapTime,
+      carName,
+      carType,
+      gapToFirst: 'gap'
+    };
 
-    if (socket && socket.readyState === WebSocket.OPEN) {
-      const data = {
-        carName,
-        lapTime,
-        driverName,
-        carType
-      };
+    sendData(newRecord); // Send new record to the WebSocket context
 
-      socket.send(JSON.stringify(data));
-      setCarName('');
-      setLapTime('');
-      setDriverName('');
-      setCarType('');
-    } else {
-      console.error('WebSocket is not connected');
-    }
+    setCarName('');
+    setLapTime('');
+    setDriverName('');
+    setCarType('');
   };
-
-  // useEffect(() => {
-  //   // Listen for incoming WebSocket messages
-  //   if (socket) {
-  //     socket.onmessage = (message) => {
-  //       const data = JSON.parse(message.data);
-  //       // Update the leaderboard with new lap data
-  //       setLeaderboardData((prevData) => {
-  //         // Add new data, ensuring unique entries
-  //         return [...prevData, data];
-  //       });
-  //     };
-  //   }
-
-  //   // Cleanup WebSocket listener on component unmount
-  //   return () => {
-  //     if (socket) {
-  //       socket.onmessage = null;
-  //     }
-  //   };
-  // }, [socket]);
 
   return (
     <div className="flex justify-evenly items-start gap-8 max-w-screen-xl mx-auto pt-24">
@@ -79,28 +42,19 @@ const Input = () => {
         <form id="lapForm" onSubmit={handleSubmit} className="flex flex-col gap-4">
           <input
             type="text"
-            id="carName"
-            value={carName}
-            onChange={handleCarNameChange}
-            placeholder="Enter car name"
-            required
-            className="p-3 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <input
-            type="text"
-            id="lapTime"
-            value={lapTime}
-            onChange={handleLapTimeChange}
-            placeholder="Enter lap time (MMDDddd)"
-            required
-            className="p-3 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <input
-            type="text"
             id="driverName"
             value={driverName}
             onChange={handleDriverNameChange}
             placeholder="Enter Driver Name"
+            required
+            className="p-3 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <input
+            type="text"
+            id="carName"
+            value={carName}
+            onChange={handleCarNameChange}
+            placeholder="Enter car name"
             required
             className="p-3 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
@@ -111,35 +65,23 @@ const Input = () => {
             id="1"
             placeholder="Select Car Type"
           />
-
+          <input
+            type="number"
+            id="lapTime"
+            value={lapTime}
+            onChange={handleLapTimeChange}
+            placeholder="Enter lap time (MMDDddd)"
+            required
+            className="p-3 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
           <button
             type="submit"
             className="p-3 bg-gray-700 text-white rounded cursor-pointer hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
             Submit
           </button>
-
-          <div className="flex justify-between mt-4">
-            <button
-              type="button"
-              id="deleteData"
-              onClick={() => console.log('Delete Data')}
-              className="w-48 p-3 bg-red-600 text-white rounded cursor-pointer hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
-            >
-              Delete
-            </button>
-            <button
-              type="button"
-              id="exportData"
-              onClick={() => console.log('Export Data')}
-              className="w-48 p-3 bg-gray-700 text-white rounded cursor-pointer hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500"
-            >
-              Export
-            </button>
-          </div>
         </form>
       </div>
-
       <Table isInputTable leaderboardData={leaderboardData} />
     </div>
   );
